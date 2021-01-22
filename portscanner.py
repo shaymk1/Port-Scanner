1d308dd5f67882521357139537bcea313787a17f
shaymk1 <69590144+shaymk1@users.noreply.github.com>
2021-01-23 00:37:22 +0200

    init

--- portscanner.py
+++ portscanner.py
@@ -2,11 +2,10 @@ import socket
 import termcolor
 
 
-def scan(target , ports):
-  print('\n' + 'Starting Scan For' + str(target))
-  for port in range(1,ports):
-    scan_port(target, port)
-
+def scan(target, ports):
+    print('\n' + 'Starting Scan For' + str(target))
+    for port in range(1, ports):
+        scan_port(target, port)
 
 
 def scan_port(ipaddress, port):
@@ -23,15 +22,13 @@ def scan_port(ipaddress, port):
         pass
         # print(" [-1]  port closed" + str(port))
 
-  #asking the user to input some IP's and ports to scan      
+    # asking the user to input some IP's and ports to scan
 targets = input("[*] Enter Targets To Scan (split them by ,) : ")
 ports = int(input("[*] Enter How Many Ports That You Want To Scan : "))
 if' , ' in targets:
-  print(termcolor.colored(("[*] Scanning multiple targets") , 'Red'))
-  for ip_addr in targets.split(','):
-      scan(ip_addr.strip(' '), ports)
+    print(termcolor.colored(("[*] Scanning multiple targets"), 'Red'))
+    for ip_addr in targets.split(','):
+        scan(ip_addr.strip(' '), ports)
 
 else:
-  scan(targets , ports)
-
-
+    scan(targets, ports)
